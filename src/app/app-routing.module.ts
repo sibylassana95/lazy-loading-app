41f50c57eccff2f6596e0b639212f630b382c147
sibylassana95 <sibyamara95@gmail.com>
2024-03-15 11:01:35 +0000

    firt commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./customers/customers.module').then((m) => m.CustomersModule),
+  },
+  {
+    path: 'service',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
 
-const routes: Routes = [];
+  {
+    path: '',
+    redirectTo: 'homes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'homes',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
